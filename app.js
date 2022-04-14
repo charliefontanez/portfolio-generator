025a1a78f27da905b19f7bc600b6065f804eb1d2
Carlos Fontanez <charliefontanezis@gmail.com>
2022-04-14 11:37:08 -0400

    setup file generator

---
+++ app.js
@@ -0,0 +1,10 @@
+const generatePage = require('./src/page-template.js');
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [name, github] = profileDataArgs;
+const fs = require('fs');
+
+fs.writeFile("index.html", generatePage(name, github), err => {
+  if (err) throw err;
+
+  console.log("Portfolio complete! Check out index.html to see the output!")
+})
