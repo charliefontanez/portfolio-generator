e5cd7f8bc7cd8a4d0b849e0dd36f1fc13c0d7b8f
Carlos Fontanez <charliefontanezis@gmail.com>
2022-04-25 20:47:26 -0400

    added css

--- utils/generate-site.js
+++ utils/generate-site.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 
 // Custom Promise
-const writeFIle = fileContent => {
+const writeFile = fileContent => {
   return new Promise((resolve, reject) => {
     fs.writeFile('./dist/index.html', fileContent, err => {
       if (err) {
@@ -19,9 +19,9 @@ const writeFIle = fileContent => {
   });
 };
 
-const copyFile = (fileName) => {
-  return new promises((resolve, reject) => {
-    fs.copyFile(fileName, '/distindex.html', err => {
+const copyFile = () => {
+  return new Promise((resolve, reject) => {
+    fs.copyFile('./src/style.css', './dist/style.css', err => {
       if (err) {
         reject(err);
         return;
